1074e8e068188fe56f13a80cc1915c83b0306841
leamor96 <116647096+leamor96@users.noreply.github.com>
2023-07-06 15:28:12 +0300

    localhost update

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const token = localStorage.getItem("token");
 
 export default axios.create({
-  baseURL: "https://lens-ease-server-production.up.railway.app/api",
+  baseURL: "http://localhost:5000/api",
   headers: {
     "Content-Type": "application/json",
     Authorization: token,
